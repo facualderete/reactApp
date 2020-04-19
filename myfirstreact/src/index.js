e8fe7fc300b8b5934607d2df3e19ffe9247d2cb2
Facundo Alderete <facualderete@gmail.com>
2020-04-19 18:35:49 -0300

    Form example

--- myfirstreact/src/index.js
+++ myfirstreact/src/index.js
@@ -1,6 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './App.css';
 
-const myfirstelement = <h1>Hello React!</h1>
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      age: null,
+      mycar: '',
+      errormessage: ''
+    };
+  }
 
-ReactDOM.render(myfirstelement, document.getElementById('root'));
+  mySubmitHandler = (event) => {
+    event.preventDefault();
+    alert("Your selected car brand is " + this.state.mycar);
+    
+  }
+
+  myChangeHandler = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    let err = '';
+    if (nam === "age") {
+      if (val !="" && !Number(val)) {
+        err = <strong>Your age must be a number</strong>;
+      }
+    }
+
+    console.log(nam);
+    console.log(val);
+
+    this.setState({errormessage: err});
+    this.setState({[nam]: val});
+  }
+  render() {
+    return (
+      <form onSubmit={this.mySubmitHandler}>
+      <h1>Hello {this.state.username} {this.state.age}</h1>
+      <p>Enter your name:</p>
+      <input
+        type='text'
+        name='username'
+        onChange={this.myChangeHandler}
+      />
+      <p>Enter your age:</p>
+      <input
+        type='text'
+        name='age'
+        onChange={this.myChangeHandler}
+      />
+      {this.state.errormessage}
+
+      <p>Select a car brand for you:</p>
+
+      <select value={this.state.mycar} name='mycar' onChange={this.myChangeHandler}>
+        <option value="Ford">Ford</option>
+        <option value="Volvo">Volvo</option>
+        <option value="Fiat">Fiat</option>
+      </select>
+
+      <p />
+
+      <input type="submit" />
+
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(<MyForm />, document.getElementById('root'));
